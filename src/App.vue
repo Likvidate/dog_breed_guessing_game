ca8736e26857805f4a786fc94f5b9a0bfb9b2ad4
Likvidate <67824760+Likvidate@users.noreply.github.com>
2021-12-15 12:11:15 +0200

    Styling

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
+  <div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body, html {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  min-height: 100vh;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+  background-color: black;
 }
 </style>
--- src/components/MainGame.vue
+++ src/components/MainGame.vue
@@ -1,13 +1,16 @@
 <template>
-  <div>
-    <button @click="getData">test</button>
-    <button @click="getImage">test</button>
-    <div>Amount of breeds: {{breedsWithSubs.length}}</div>
-    {{currentName}}
-    <img :src="`${img}`">
-    <ul v-for="breed in breedsWithSubs" v-bind:key="breed">
-      <li>{{breed}}</li>
-    </ul>
+  <div class="container">
+    <div class="img"></div>
+    <div class="img"><img :src="`${img}`"></div>
+    <div class="img"></div>
+    <div class="buttons">
+      <button class="btn" @click="getImage">test 1</button>
+      <button class="btn">test 2</button>
+      <button class="btn">test 3</button>
+      <button class="btn">test 4</button>
+    </div>
+    <div class="stats">
+    </div>
   </div>
 </template>
 
@@ -28,26 +31,6 @@ export default {
     }
   },
   methods: {
-    getData () {
-      axios.get('https://dog.ceo/api/breeds/list/all').then((res) => {
-        this.breedList = res.data.message
-        Object.keys(this.breedList).forEach(breed => this.breeds.push(breed))
-        this.breeds.forEach(breed => {
-          axios.get('https://dog.ceo/api/breed/' + breed + '/list').then((res) => {
-            if (res.data.message.length === 0) {
-              this.breedsWithSubs.push(breed)
-            } else {
-              res.data.message.forEach(sub => {
-                  this.breedsWithSubs.push(sub + ' ' + breed)
-              })
-            }
-          })
-        })
-      })
-      .catch((error) => {
-        console.error(error)
-      })
-    },
     getImage () {
       var randomBreed = null
       var randomInt = parseInt(Math.random() * ((this.breeds.length - 1) - 0 + 1), 10)  + 0
@@ -62,14 +45,70 @@ export default {
         }
       })
     }
+  },
+  created () {
+    axios.get('https://dog.ceo/api/breeds/list/all').then((res) => {
+      this.breedList = res.data.message
+      Object.keys(this.breedList).forEach(breed => this.breeds.push(breed))
+      this.breeds.forEach(breed => {
+        axios.get('https://dog.ceo/api/breed/' + breed + '/list').then((res) => {
+          if (res.data.message.length === 0) {
+            this.breedsWithSubs.push(breed)
+          } else {
+            res.data.message.forEach(sub => {
+                this.breedsWithSubs.push(sub + ' ' + breed)
+            })
+          }
+        })
+      })
+    })
+    .catch((error) => {
+      console.error(error)
+    })
   }
 }
 </script>
 
 <style>
+.img {
+  flex: 0 0 33%;
+}
+.stats {
+  flex: 0 0 100%;
+}
 img {
   object-fit: cover;
-  width: 50%;
+  width: 100%;
   height: 700px;
+}
+.container {
+  margin: 5%;
+  justify-content: center;
+  display: flex;
+  flex-wrap: wrap;
+  background-color: black;
+}
+.buttons {
+  margin: 2%;
+  justify-content: center;
+  display: flex;
+  flex-wrap: wrap;
+  width: 600px;
+
+}
+.btn {
+    flex: 0 0 48%;
+    outline: none;
+    background: none;
+    color: white;
+    border: 1px solid aqua;
+    padding: 10px 20px;
+    margin-right: 10px;
+}
+.btn:hover {
+  cursor: pointer;
+  color: white;
+  border: 1px solid aqua;
+  background-color: aqua;
 }
 </style>
