f632670e002596623b2758f9266636d1b601c26a
Irmantas.Č <67824760+Likvidate@users.noreply.github.com>
2022-01-06 16:00:08 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/dog_breed_guessing_game
-/",
+  publicPath: process.env.NODE_ENV === "production" ? "/dog_breed_guessing_game/",
 };
